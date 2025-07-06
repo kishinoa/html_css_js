caec3594af3d9a915acc6da02b5641e4659b15cf
FOJF <john9823@naver.com>
2025-07-06 19:25:01 +0900

    다른 위치의 스크립트 부분이라도 하나의 HTML내 에서 같은 변수명을 사용하면 공유하는 거 같음

--- js/myjs.js
+++ js/myjs.js
@@ -1,14 +1,19 @@
-var isFirst = false;
+var isFirst3 = false;
 var original3 = null;
 
 function updateF3() {
-    if (!isFirst) {
-        original3 = document.getElementById("f3").innerText;
-        isFirst = true;
-    }
+    getOriginalF3();
     document.getElementById("f3").innerHTML = "f3가 변경 됨"
 }
 
 function restoreF3() { 
+    getOriginalF3();
     document.getElementById("f3").innerHTML = original3;
+}
+
+function getOriginalF3() {
+    if (!isFirst3) {
+        original3 = document.getElementById("f3").innerText;
+        isFirst3 = true;
+    }
 }
